73234de5f60d0275ece1374271f2ee4c4ee1b334
dimdos <dimdos@hotmail.com>
2017-01-10 17:48:49 +0200

    found square a*a

--- Program.java
+++ Program.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 public class Program
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World !");
 		System.out.println("Happy new year");
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		System.out.println(a*a);
+		
 	}
 }
